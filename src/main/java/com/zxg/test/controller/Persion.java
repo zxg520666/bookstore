20a70e8ed70a4890aa9a418cfe76474723a4173d
zxg <zxg@qq.com>
2020-07-25 18:39:20 +0800

    zxg 修改啦

--- src/main/java/com/zxg/test/controller/Persion.java
+++ src/main/java/com/zxg/test/controller/Persion.java
@@ -2,6 +2,7 @@ package com.zxg.test.controller;
 
 public class Persion {
     public static void main(String[] args) {
+        System.out.println("99999");
         System.out.println(1111888669);
 
     }
