72a33c4117280885c9bcc5227e6270ff772d9d14
bht <bht@qq.com>
2020-07-25 17:44:29 +0800

    bht 的修改的代码

--- src/main/java/com/zxg/test/controller/TestGitMeager.java
+++ src/main/java/com/zxg/test/controller/TestGitMeager.java
@@ -2,7 +2,7 @@ package com.zxg.test.controller;
 
 public class TestGitMeager {
     public static void main(String[] args) {
-
+        System.out.println("8888");
         System.out.println("99999");
         System.out.println("zxg修改的");
     }
